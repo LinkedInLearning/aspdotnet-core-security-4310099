5aaf13fe5929243a8999318624b328cbd0dc920b
Ervis Trupja <ervistrupja@outlook.com>
2022-12-15 21:31:54 +0100

    initial code

---
+++ SecurityApp/SecurityApp.API/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace SecurityApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("{userId}")]
+        public IActionResult GetUserById(string userId)
+        {
+            if (string.IsNullOrEmpty(userId))
+                return BadRequest();
+
+            string sql = "SELECT * FROM users WHERE id = " + userId;
+            using (SqlCommand cmd = new SqlCommand(sql, new SqlConnection()))
+            {
+                //Excute command in here
+                cmd.ExecuteReader();
+            }
+
+            return Ok();
+        }
+    }
+}
