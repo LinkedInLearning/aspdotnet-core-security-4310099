27632b7a6c991f01eaf2fcb3db39fa3faa2659fb
Ervis Trupja <ervistrupja@outlook.com>
2022-12-15 23:06:31 +0100

    Add authentication related files

---
+++ SecurityApp/SecurityApp.API/Controllers/AuthenticationsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SecurityApp.API.Models;
+using SecurityApp.API.ViewModels;
+
+public class AccountController : Controller
+{
+    private readonly UserManager<User> _userManager;
+    private readonly SignInManager<User> _signInManager;
+
+    public AccountController(
+        UserManager<User> userManager,
+        SignInManager<User> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login([FromBody] LoginViewModel model)
+    {
+        var user = await _userManager.FindByEmailAsync(model.Email);
+        if (user == null)
+        {
+            return BadRequest("Invalid email or password.");
+        }
+
+        // Check if the user is locked out
+        if (await _userManager.IsLockedOutAsync(user))
+        {
+            return BadRequest("Your account has been locked out. Please try again later.");
+        }
+
+        // Check if the user provided the correct password
+        var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+        if (!result.Succeeded)
+        {
+            // Increment the failed access attempt count for the user and lock them out if necessary
+            return BadRequest("Invalid email or password.");
+        }
+
+        // Sign the user in and return the access token
+        var token = await GenerateJwtToken(user);
+        return Ok(new { token });
+    }
+
+    private Task<string> GenerateJwtToken(User user)
+    {
+        throw new NotImplementedException();
+    }
+}
