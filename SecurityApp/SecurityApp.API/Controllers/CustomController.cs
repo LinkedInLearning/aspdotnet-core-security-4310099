b4c73784fbcc3a525d7021c811c5071b9399e82c
Ervis Trupja <ervistrupja@outlook.com>
2022-12-18 00:34:28 +0100

    Added API-Key Authentication example

---
+++ SecurityApp/SecurityApp.API/Controllers/CustomController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SecurityApp.API.Filters;
+
+namespace SecurityApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ApiKeyAuth]
+    public class CustomController : ControllerBase
+    {
+    }
+}
---
+++ SecurityApp/SecurityApp.API/Filters/ApiKeyAuthAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace SecurityApp.API.Filters
+{
+    public class ApiKeyAuthAttribute: ActionFilterAttribute
+    {
+        private const string CustomApiKeyParam = "X-API-Key";
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var apiKey = context.HttpContext.Request.Headers.TryGetValue(CustomApiKeyParam, out var apiKeyValue);
+
+            if (!apiKey)
+            {
+                context.Result = new UnauthorizedObjectResult("X-API-Key is missing in the header");
+            }
+
+            // apiKeyValue check
+            //if(apiKeyValue //does not exist)
+            //    context.Result = new UnauthorizedObjectResult("X-API-Key is not valid");
+
+            //if(apiKeyValue rate < //limit for hte apiKey)
+            //    context.Result = new UnauthorizedObjectResult($"{apiKeyValue} has exceeded the daily limit");
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
