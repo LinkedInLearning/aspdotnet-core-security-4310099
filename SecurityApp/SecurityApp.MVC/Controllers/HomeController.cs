01413f9d57f3da5dc460574d2df5c52cbf5355cf
Ervis Trupja <ervistrupja@outlook.com>
2022-12-17 22:21:01 +0100

    added [Authorize] attribute

--- SecurityApp/SecurityApp.MVC/Controllers/HomeController.cs
+++ SecurityApp/SecurityApp.MVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SecurityApp.MVC.Models;
 using System.Diagnostics;
 
@@ -18,6 +19,7 @@ namespace SecurityApp.MVC.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();
